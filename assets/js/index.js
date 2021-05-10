69cbe81a71588e602f976da8b082dbc87a7852ab
Cal Withington <scwithington@gmail.com>
2021-05-10 15:02:11 -0400

    resume page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,6 @@
-// let images = ['./assets/img/resume.jpg', './assets/img/portfolio.jpg'];
-// let i = 0;
-
-// setInterval(function() {
-//     $('#pageLead').animate({ opacity: 0 }, 500, function() {
-//         $('#pageLead').css('background-image', 'url(' + images[i] + ')');
-//         i++;
-//         $('#pageLead').animate({ opacity: 1 }, 500, function() {
-//             if (i == images.length) {
-//                 i = 0;
-//             }
-//         });
-//     });
-// }, 5000);
-
 (function slide() {
-    var i = 1;
-    var classes = ['bgd-1', 'bgd-2'];
+    var i = 0;
+    var classes = ['bgd-1', 'bgd-2', 'bgd-3'];
     var el = document.getElementById('pageLead');
     function toggle() {
         el.className = classes[i];
