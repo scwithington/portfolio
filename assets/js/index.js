db69558eba71237082d94d36aea85d7ed0db6ce9
Cal Withington <scwithington@gmail.com>
2021-05-10 11:42:41 -0400

    slideshow looks ok.. testing jquery ui for better transition

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,14 @@
-let images = ['resume.jpg', '']
+let images = ['./assets/img/resume.jpg', './assets/img/portfolio.jpg'];
+let i = 0;
+
+setInterval(function() {
+    $('#pageLead').animate({ opacity: 0 }, 500, function() {
+        $('#pageLead').css('background-image', 'url(' + images[i] + ')');
+        i++;
+        $('#pageLead').animate({ opacity: 1 }, 500, function() {
+            if (i == images.length) {
+                i = 0;
+            };
+        });
+    });
+}, 5000);
